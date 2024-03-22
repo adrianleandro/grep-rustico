4ec3bf7b41cd28291d5389c44efe917007bc0bcb
Adrian <are@fi.uba.ar>
2024-03-22 19:54:19 -0300

    Agrego tests y termino main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::env::args as argv;
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = argv().collect();
+    
+    if argv().count() != 3 {
+        if let Some(nombre_programa) = args.get(0) {
+            eprintln!("Uso: {} [expresión regular] [archivo]", nombre_programa);
+        }
+        return;
+    }
+
+    //let archivo = abrir_archivo(args.get(2));
+    //egrep(args.get(1), archivo);
+    return;
 }
---
+++ tests/tests.rs
@@ -0,0 +1,25 @@
+#[cfg(test)]
+mod tests{
+    //use super::*;
+
+    #[test]
+    fn period(){
+        assert_eq!(".", ".");
+    }
+
+    #[test]
+    fn period_with_asterisc(){
+        assert_eq!(".*", ".*");
+    }
+
+    #[test]
+    fn bracket_expression(){
+        assert_eq!("[]", "[]");
+    }
+
+    #[test]
+    fn curly_bracket_expression(){
+        assert_eq!("{}", "{}");
+    }
+    
+}
